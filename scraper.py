b4d08c12703945d70bbe03d67cd140f17f413aea
小龙 <lilongsy@126.com>
2016-06-16 16:32:53 +0800

    fixed db bug
    fixed db bug

--- scraper.py
+++ scraper.py
@@ -7,13 +7,15 @@ from multiprocessing import Pool
 import MySQLdb
 import re
 import configparser
-import os
+
+# 采集地址
+root_url = 'http://news.sdchina.com'
+list_url = root_url + '/list/1227.html'
 
 # 数据库连接
-config = configparser.ConfigParser()
-config.read('db.ini')
-db = config['db']
-conn = MySQLdb.connect(host=db['db_host'], user=db['db_user'], passwd=db['db_pass'], db=db['db_database'], port=int(db['db_port']), charset='utf8')
+dbc = configparser.ConfigParser()
+dbc.read('db.ini')
+conn = MySQLdb.connect(host=dbc.get("db", 'db_host'), user=dbc.get("db", 'db_user'), passwd=dbc.get("db", 'db_pass'), db=dbc.get("db", 'db_database'), port=int(dbc.get("db", 'db_port')), charset='utf8')
 cur = conn.cursor(MySQLdb.cursors.DictCursor)
 
 # 插入数据库数据
@@ -32,89 +34,50 @@ def filter_tags(htmlstr):
     s = re_iframe.sub('', s)
     return s
 
-def main():
-    for parent,dirnames,filenames in os.walk("rules/"):
-        for filename in filenames:
-            if filename[-4:] == '.ini':
-                config.read("rules/" + filename)
-                li = config['list']
-                page_urls = get_page_urls(li['url'], li['select'])
-                
-                for page_url in page_urls:
-                    try:
-                        db_insert(get_page_data(page_url, config))
-                    except Exception,e:
-                        print e
-                        pass
-                        
-                conn.commit()
-                cur.close()
-                conn.close()
-                    
-    return
-
 # 获取列表页文章链接
-def get_page_urls(list_url, select):
+def get_page_urls():
     response = requests.get(list_url)
     soup = bs4.BeautifulSoup(response.text, "html.parser")
-    return  [a.attrs.get('href') for a in soup.select(select)]
+    return  [a.attrs.get('href') for a in soup.select('.zlefta h3 a')]
 
 # 获取内容页内容
-def get_page_data(page_url, config):
-
+def get_page_data(page_url):
     data = {}
-    data['url'] = page_url
-    
-    sections = config.sections()
     response = requests.get(page_url)
     soup = bs4.BeautifulSoup(response.text, "html.parser")
-
-    for section in sections:
-        if section != 'list' and section != 'db':
-            try:
-                select = config[section]['select']
-                index = int(config[section]['index'])
-                print select
-                
-                if 'attr' in config[section]:
-                    attr = config[section]['attr']
-                    data[section] = soup.select(select)[index].attrs.get(attr)
-                else:
-                    # html格式
-                    if 'format' in config[section]:
-                        data[section] = filter_tags(soup.select(select)[index].prettify())
-                        
-                    # 文本格式
-                    else:
-                        data[section] = soup.select(select)[index].get_text()
-                        
-                        # 截取字符开始
-                        if 'start' in config[section]:
-                            start = config[section]['start']
-                            start_offset = int(data[section].find(start) + len(start))
-                            print 'Start:' + config[section]['start']
-                            print 'Start Offset:' + str(start_offset)
-                        else:
-                            start_offset = None
-                        
-                        # 截取字符结束
-                        if 'end' in config[section]:
-                            end = config[section]['end']
-                            end_offset = int(data[section].find(end))
-                            print 'End:' + config[section]['end']
-                            print 'End Offset:' + str(end_offset)
-                        else:
-                            end_offset = None
-                        
-                        if(start_offset is not None or end_offset is not None):
-                            data[section] = data[section][start_offset:end_offset].strip()
-                    #print data[section]
-                            
-            except IndexError,e:
-                print e
-                pass
-                
+    data['url'] = page_url
+    data['title'] = soup.select('h1')[1].get_text()
+    data['keywords'] = soup.select('meta[name="keywords"]')[0].attrs.get('content')
+    data['description'] = soup.select('meta[name="description"]')[0].attrs.get('content')
+    pt = soup.select('.zleftc')[0].get_text()
+    data['publishtime'] = pt[:pt.find(u'来源')].strip()
+    source = soup.select('.zleftc')[0].get_text()
+    data['source'] = source[source.find(u'来源：')+3:].strip()
+    data['content'] = filter_tags(soup.select('.zleftf')[0].prettify())
+    images = soup.select('.zleftf img')
+    if images:
+        data['image'] = images[0].attrs.get('src')
+    else:
+        data['image'] = ''
+    data['author'] = soup.select('.zleftg')[0].get_text()
     return data
 
+# 入口
+def show_stats():
+    # pool = Pool(8)
+    page_urls = get_page_urls()
+    #results = pool.map(get_page_data, page_urls)
+
+    for page_url in page_urls:
+        try:
+            data = get_page_data(page_url)
+            db_insert(data)
+        except Exception,e:
+            print e
+            continue
+    conn.commit()
+    cur.close()
+    conn.close()
+
 if __name__=='__main__':
-    main()
+    show_stats()
